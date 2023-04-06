40f3c43a48f398b516618e9ae273ebe46fb6c8a9
Crowdbotics <engineering@crowdbotics.com>
2023-04-06 01:57:16 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/morning_tooth_dev_74786/MainActivity.java
@@ -0,0 +1,15 @@
+package com.morning_tooth_dev_74786;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "morning_tooth_dev_74786";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "morning-tooth-dev-74786",
+  url: "https://morning-tooth-dev-74786.botics.co",
+  api: "https://morning-tooth-dev-74786.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
